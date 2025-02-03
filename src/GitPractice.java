4e2db156b5b3706740480aa3bd6e93a6349afee4
vinithsiva <128915898+vinithsiva@users.noreply.github.com>
2025-02-03 09:36:57 +0530

    Update GitPractice.java

--- src/GitPractice.java
+++ src/GitPractice.java
@@ -3,5 +3,6 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("Git Practice");
 		System.out.println("Git Practice - merge conflict");
+		System.out.println("Git Practice - merge conflict - 1");
 	}
 }
